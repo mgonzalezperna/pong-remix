4aa21436cc33e5c4e999d6818a7669ee56ca9015
Martin Gonzalez Perna <mgonzalezperna@gmail.com>
2020-06-04 17:32:55 -0300

    Tests on how ball handle. Still WIP.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -8,6 +8,7 @@ func _ready():
 
 # Function to manipulate position safetly without corrupting physics engine.
 func _integrate_forces(state):
+    state.set_angular_velocity(0)
     var ball_state = ball_state_on_screen(state)
     state.set_transform(ball_state)
 
@@ -20,9 +21,17 @@ func ball_state_on_screen(state):
         ball_state.origin.x = screen_size.x
     return ball_state
 
-
-func _process(delta):
+func _physics_process(delta):
     var collition_bodies = get_colliding_bodies()
     for body in collition_bodies:
-        if body.get_name() == "paddle_player1":
-            pass
+        if body.get_name() in ["paddle_player1", "paddle_player2"]:
+            if body.position.x != self.position.x:
+                var impulse_x = body.get_velocity().x
+                var impulse_y = body.get_velocity().y
+                self.apply_central_impulse(Vector2(body.get_velocity()))
+
+#    var collision = move_and_collide(velocity * delta)
+#    if collision:
+#        velocity = velocity.bounce(collision.normal)
+#        if collision.collider.has_method("hit"):
+#            collision.collider.hit()
