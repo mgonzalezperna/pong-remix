d4919360f616b0ef456d7c010f59528f5041842a
eXtragoZ <jjllnn_kk@hotmail.com>
2020-06-04 20:59:33 -0300

    paddle angle rotation clamp

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 var screen_size
 export(float, 0, 1) var paddle_angle_override = 0.5
-
+export(int, 0, 180) var max_rotation_override = 45
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -26,9 +26,10 @@ func _process(delta):
 	var collition_bodies = get_colliding_bodies()
 	for body in collition_bodies:
 		if body.get_name() in ["paddle_player1", "paddle_player2"]:
-			var hit_vector = body.position.direction_to(self.position)
+			# var shape = get_node(NodePath("../" + body.get_name() + "/CollisionShape2D")).get_shape()
+			var paddleOffset = body.get_name() == "paddle_player1" if -40 else 40
+			var hit_vector = (body.position + Vector2(paddleOffset, 0)).direction_to(self.position)
 			var rotation = self.linear_velocity.angle_to(hit_vector) * paddle_angle_override
-			print('hit_angle ', hit_vector.angle())
-			print('linear_velocity ', self.linear_velocity.angle())
-			print('rotation ', rotation)
+			print('rotation ', rad2deg(rotation))
+			rotation = clamp(rotation, -deg2rad(max_rotation_override), deg2rad(max_rotation_override))
 			self.linear_velocity = self.linear_velocity.rotated(rotation)
